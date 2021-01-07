6efef9c23d1d6a15d9d6612d10278645a984d2f2
Ted Thompson <ted@federalproductions.com>
2021-01-07 11:48:05 -0800

    Update run_me.bat
    Added lines to alter Title Bar text
    Wrapped long echo lines

--- run_me.bat
+++ run_me.bat
@@ -17,12 +17,14 @@ if "%errorLevel%" == "0" (
   netsh advfirewall show currentprofile state | find "ON" >nul 2>&1
   echo netsh "!errorLevel!"
   if not "!errorLevel!" == "0" (
-    echo Error: You need to enable Windows Firewall
+    echo Error: You need to enable 
+	echo Windows Firewall
     pause
     exit /b
   )
 ) else (
-  echo Error: You need to start Windows Firewall service
+  echo Error: You need to start
+  echo Windows Firewall service
   pause
   exit /b
 )
@@ -36,10 +38,12 @@ cls
 netsh advfirewall firewall show rule name=%fwRule% >nul 2>&1
 if not %errorLevel% == 1 (
     netsh advfirewall firewall delete rule name=%fwRule%
-    echo.Firewall rule deleted. The public can now join your session
+    echo.Firewall rule deleted. The public can now
+	echo.join your session.
 )
 
 :menuNone
+title Public
 echo.
 echo =========================================
 echo GTA V: Online private session by reverie
@@ -50,6 +54,7 @@ echo 2) Start whitelist session
 echo 5) How to use
 echo 7) About
 echo 0) Exit
+echo.
 set m=x
 set /p m=Enter option: 
 if %m%==1 goto startSolo
@@ -62,12 +67,14 @@ echo.Error: Invalid option selected
 goto menuNone
 
 :menuSolo
+title Solo
 echo.
 echo =========================================
 echo GTA V: Online private session by reverie
 echo =========================================
 echo.
 echo 1) Stop solo public session
+echo.
 set m=x
 set /p m=Enter option: 
 if %m%==1 goto deleteFW
@@ -76,6 +83,7 @@ echo.Error: Invalid option selected
 goto menuSolo
 
 :menuWhitelist
+title White
 set refreshWhitelist=0
 echo.
 echo =========================================
@@ -84,6 +92,7 @@ echo =========================================
 echo.
 echo 2) Stop whitelist session
 echo 5) Refresh/View whitelist
+echo.
 set m=x
 set /p m=Enter option: 
 if %m%==2 goto deleteFW
@@ -101,28 +110,31 @@ echo.-----------------------------
 echo.How to play alone
 echo.-----------------------------
 echo.1. Go to GTA V: Online.
-echo.2. After entering a public session, select "1) Start solo public session".
-echo.3. Wait for a few moments for players to leave the session. Find a new session if not.
-echo.4. ???
-echo.5. Profit^^!
+echo.2. Entering a public session, select 
+echo.   "1) Start solo public session".
+echo.3. Wait for a few moments for players to
+echo    leave the session. Find a new session if
+echo.   not.
 echo.
 echo.-----------------------------
 echo.How to play with friends
 echo.-----------------------------
-echo.1. Ensure your friends' IP are put in a text file called "whitelist.txt" in the same directory where this program is run. They can find their IP address through any third-party services such as www.canihazip.com or even simply googling "what is my ip".
+echo.1. Ensure your friends' IP are put in a 
+echo.   text file called "whitelist.txt".
 echo.2. Follow steps in "Play alone".
-echo.3. Select "1) Stop solo public session" and then "2) Start whitelist session".
-echo.4. Invite your friends into your session via Steam overlay or Rockstar Social Club.
-echo.5. ???
-echo.6. Profit^^!
+echo.3. Select "1) Stop solo public session" 
+echo.   and then "2) Start whitelist session".
+echo.4. Invite your friends into your session 
+echo.   via Steam overlay or Social Club.
 echo.
 echo.-----------------------------
 echo.How to remove firewall rules
 echo.-----------------------------
-echo.1. Select "Stop whitelist session" or "Stop solo public session".
+echo.1. Select "Stop whitelist session" or
+echo.   "Stop solo public session".
 echo.
-echo.Closed the program without selecting that first? The next time you run this they will be deleted automatically.
-echo.
+pause
+cls
 goto menuNone
 
 :readme
@@ -130,20 +142,11 @@ cls
 echo.-----------------------------
 echo.About
 echo.-----------------------------
-echo.This tool uses *Windows Firewall* to set up port blocking and whitelisting so you can have a fun experience playing GTA V: Online alone or with your friends. You must enable it first if you want to use this tool.
-echo.
-echo.This tool is not compatible if you are using a third-party firewall such as ZoneAlarm or Comodo.
-echo.
-echo.This tool is useful if you want to play features such as CEO missions and Biker businesses that can otherwise only be played if you are in a Public session but wish to keep out griefers and/or modders whose sole intention are to ruin your game experience.
-echo.
-echo.This is a tool written for convenience:
-echo.1. You do not need account registration with a third-party website.
-echo.2. You do not need to bother with the tedious configuration of Windows Firewall rules.
-echo.
-echo.This tool does not touch any files related to the game:
-echo.1. Instead it manipulates your computer's connection to the network so you cannot connect to other players' sessions.
-echo.2. When you cannot connect to any network (same as if you have a terrible internet connection), the game places you into a public session by yourself.
-echo.3. Unless Rockstar Games decides to change their terms of service, there is no reason for your account to be flagged and banned as a result of using this tool.
+echo.This tool uses *Windows Firewall* to set up
+echo.port blocking and whitelisting so you can 
+echo.have a fun experience playing GTA V: Online 
+echo.alone or with your friends. You must enable
+echo.it first if you want to use this tool.
 echo.
 goto menuNone
 
@@ -152,7 +155,8 @@ cls
 REM Create firewall rule for solo session
 netsh advfirewall firewall add rule name=%fwRule% protocol=UDP dir=out action=block localport=6672
 netsh advfirewall firewall add rule name=%fwRule% protocol=UDP dir=in action=block localport=6672
-echo.Firewall rules set. You are now in solo public session.
+echo.Firewall rules set. You are now in solo 
+echo public session.
 goto menuSolo
 
 :startWhitelist
@@ -161,7 +165,10 @@ REM Check for whitelist.txt
 if not exist %InputFile% (
     echo.
     echo.
-    echo Error: You need to create whitelist.txt file in the same directory where this program is run
+    echo Error: You need to create whitelist.txt file
+	echo.in the same directory where this program is 
+	echo.run.
+	echo.
     goto menuNone
 )
 
@@ -266,7 +273,8 @@ del "%TmpFile%"
 REM Create firewall rule for whitelist session based on range to block
 netsh advfirewall firewall add rule name=%fwRule% protocol=UDP dir=out action=block localport=6672 remoteip=!fullRange!
 netsh advfirewall firewall add rule name=%fwRule% protocol=UDP dir=in action=block localport=6672 remoteip=!fullRange!
-echo.Firewall rules set. Only players from the whitelist can now join your session
+echo.Firewall rules set. Only players from the whitelist can now join 
+echo.your session
 goto menuWhitelist
 
 :quitScript
